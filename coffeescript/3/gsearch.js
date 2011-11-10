3828b4172f57a366986f3a1d8c590d0659a56fb2
Michael Galpin <michael@bu.mp>
2011-11-09 21:45:29 -0800

    Initial commit for article 3

---
+++ coffeescript/3/gsearch.js
@@ -0,0 +1,84 @@
+{"responseData": 
+	{"results":[
+		{
+			"GsearchResultClass":"GwebSearch",
+			"unescapedUrl":"http://jashkenas.github.com/coffee-script/",
+			"url":"http://jashkenas.github.com/coffee-script/",
+			"visibleUrl":"jashkenas.github.com",
+			"cacheUrl":"http://www.google.com/search?q\u003dcache:nuWrlCK4-v4J:jashkenas.github.com",
+			"title":"\u003cb\u003eCoffeeScript\u003c/b\u003e",
+			"titleNoFormatting":"CoffeeScript",
+			"content":"\u003cb\u003eCoffeeScript\u003c/b\u003e is a little language that compiles into JavaScript. Underneath all of   those embarrassing braces and semicolons, JavaScript has always had a \u003cb\u003e...\u003c/b\u003e"
+		},
+		{
+			"GsearchResultClass":"GwebSearch",
+			"unescapedUrl":"https://github.com/jashkenas/coffee-script",
+			"url":"https://github.com/jashkenas/coffee-script",
+			"visibleUrl":"github.com",
+			"cacheUrl":"http://www.google.com/search?q\u003dcache:AakNlmNIIBYJ:github.com",
+			"title":"jashkenas/\u003cb\u003ecoffee-script\u003c/b\u003e - GitHub",
+			"titleNoFormatting":"jashkenas/coffee-script - GitHub",
+			"content":"Read+Write access. \u003cb\u003ecoffee-script\u003c/b\u003e /. Dismiss Octotip: You\u0026#39;ve activated the \u003cb\u003e...\u003c/b\u003e"
+		},
+		{
+			"GsearchResultClass":"GwebSearch",
+			"unescapedUrl":"http://en.wikipedia.org/wiki/CoffeeScript",
+			"url":"http://en.wikipedia.org/wiki/CoffeeScript",
+			"visibleUrl":"en.wikipedia.org",
+			"cacheUrl":"http://www.google.com/search?q\u003dcache:wshlXQEIrhIJ:en.wikipedia.org",
+			"title":"\u003cb\u003eCoffeeScript\u003c/b\u003e - Wikipedia, the free encyclopedia",
+			"titleNoFormatting":"CoffeeScript - Wikipedia, the free encyclopedia",
+			"content":"\u003cb\u003eCoffeeScript\u003c/b\u003e is a programming language that transcompiles to JavaScript. The   language adds syntactic sugar inspired by Ruby, Python and Haskell to enhance \u003cb\u003e...\u003c/b\u003e"
+		},
+		{
+			"GsearchResultClass":"GwebSearch",
+			"unescapedUrl":"http://codelikebozo.com/why-im-switching-to-coffeescript",
+			"url":"http://codelikebozo.com/why-im-switching-to-coffeescript",
+			"visibleUrl":"codelikebozo.com",
+			"cacheUrl":"http://www.google.com/search?q\u003dcache:VDKirttkw30J:codelikebozo.com",
+			"title":"Why I\u0026#39;m (Finally) Switching to \u003cb\u003eCoffeeScript\u003c/b\u003e - Code Like Bozo",
+			"titleNoFormatting":"Why I\u0026#39;m (Finally) Switching to CoffeeScript - Code Like Bozo",
+			"content":"Sep 5, 2011 \u003cb\u003e...\u003c/b\u003e You may have already heard about \u003cb\u003eCoffeeScript\u003c/b\u003e and some of the hype   surrounding it but you still have found several reasons to not make the \u003cb\u003e...\u003c/b\u003e"
+		}],
+	"cursor":{
+		"resultCount":"75,100",
+		"pages":[
+			{
+				"start":"0",
+				"label":1
+			},
+			{
+				"start":"4",
+				"label":2
+			},
+			{
+				"start":"8",
+				"label":3
+			},
+			{
+				"start":"12",
+				"label":4
+			},
+			{
+				"start":"16",
+				"label":5
+			},
+			{
+				"start":"20",
+				"label":6
+			},
+			{
+				"start":"24",
+				"label":7
+			},
+			{
+				"start":"28",
+				"label":8
+			}],
+		"estimatedResultCount":"75100",
+		"currentPageIndex":0,
+		"moreResultsUrl":"http://www.google.com/search?oe\u003dutf8\u0026ie\u003dutf8\u0026source\u003duds\u0026start\u003d0\u0026hl\u003den\u0026q\u003dcoffeescript",
+		"searchResultTime":"0.07"}}, 
+	"responseDetails": null, 
+	"responseStatus": 200
+}
---
+++ coffeescript/3/search.coffee
@@ -0,0 +1,25 @@
+class SearchResult
+	constructor: (data) ->
+		@title = data.title
+		@link = data.link
+		@extras = data
+	
+	toHtml: () -> "<a href='#{@link}'>#{@title}</a>"
+	toJson: () -> JSON.stringify @extras
+
+class GoogleSearchResult
+	toHtml: () ->
+		"#{super.toHtml()} <div class='snippet'>#{@extras.snippet}</div>"
+			
+class TwitterSearchResult
+	toHtml: () ->
+		"<a href='http://twitter.com/#{@extras.from_user}'>@#{@extras.from_user}</a>: #{super.toHtml()}"
+		
+class MockSearch
+	search: (query) ->
+		results = 
+			google: (new GoogleSearchResult obj for obj in mockGoogleData)
+			twitter: (new TwitterSearchResult obj for obj in mockTwitterData)
+		results
+
+
